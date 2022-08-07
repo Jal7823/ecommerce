3a5d5bd35da68e1e1f3d601c22c22cb7b2c558f9
Jal <70490845+Jal7823@users.noreply.github.com>
2022-08-07 15:19:25 -0300

    Update settings.py

--- casaNorma/settings.py
+++ casaNorma/settings.py
@@ -17,8 +17,8 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEBUG')
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
+# DEBUG = False
 
 
 
